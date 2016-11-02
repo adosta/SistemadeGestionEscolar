da8774884ac300d9e3619c49989598e10c6966f9
adosta <adrianboy127@hotmail.com>
2016-11-02 15:19:59 -0700

    Cambios

---
+++ SistemadeGestionEscolar/Models/Profesor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SistemadeGestionEscolar.Models
+{
+    public class Profesor
+    {
+        [Key]
+        public int profesorID { get; set; }
+
+        [Required]
+        [MinLength(1)]
+        [Display(Name = "Nombre")]
+        public string nombre { get; set; }
+
+        [Required]
+        [Display(Name = "Apellido Paterno")]
+        public string apellidoPaterno { get; set; }
+
+        [Required]
+        [Display(Name = "Apellido Materno")]
+        public string apellidoMaterno { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
+        public DateTime fechaDeNacimiento { get; set; }
+
+        public virtual ICollection<Grupo> grupos { get; set; }
+    }
+}
